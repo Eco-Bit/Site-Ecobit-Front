32bbbf80c420401d395834aa5e691f63a0028da9
Lucas Davi <111915416+EuLucasDavi@users.noreply.github.com>
2024-05-14 16:11:17 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,11 @@ import AppRoutes from "./Routes";
 
 function App() {
 
-  //teste2
-  //
   return (
     <div className="app">
       <AppRoutes/>
     </div>
   )
 }
-
-
-
-//aksnlaskndlads
-//açsdmkaçlsdmasçdl
-//aslkdmaskdma
-
 
 export default App;
